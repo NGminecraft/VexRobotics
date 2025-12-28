3c26d9d6c6908c966425ce9ab3aea0226e3cd73e
NGminecrafter <grahanic001@alpinesd.org>
2025-12-27 22:51:20 -0700

    Finishing rotation sensor

--- src/Objects/RotationSensor.cpp
+++ src/Objects/RotationSensor.cpp
@@ -1,3 +1,7 @@
 #include "Objects/RotationSensor.h"
 
 RotationSensorState::RotationSensorState(vex::rotation& r) : StateObject<vex::rotation>(r), angle(r) {}
+
+RotationSensorState::update(unsigned long tick) {
+	angle.update(tick);
+}
