80700a498c470b1141c254d9c487d4a44856307f
NGminecrafter <grahanic001@alpinesd.org>
2025-12-28 23:50:13 -0700

    Starting arm

---
+++ include/Object Groups/Motors/Arm.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Object Groups/Motors/ArmSegment.h"
+#include <vector>
+
+class Arm {
+public:
+	Arm(ArmSegment& Arms, size_t length);
+
+	void AddSegment(ArmSegment& segment);
+
+
+
+private:
+	std::vector<ArmSegment&> segments;
+};
