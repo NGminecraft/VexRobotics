326c92cd1356211be23b3eba286bbacdb6118f2c
NGminecrafter <grahanic001@alpinesd.org>
2026-01-02 01:37:14 -0700

    A (much better) vector class that can hold either pointers or raw data

--- include/Data Types/Vectors.h
+++ include/Data Types/Vectors.h
@@ -3,28 +3,110 @@
 
 #include <concepts>
 #include <type_traits>
+#include <initializer_list>
+#include <array>
 
-template <Numeric T, size_t N>
-class Vector {
+template <Numeric T, size_t N, bool Owner>
+class VectorBase {
 public:
-	Vector() : data{} {}
+	VectorBase() : data{} {}
 
-	template <typename ... Args>
-	requires (sizeof...(args) == N)
-	Vector(args... args) : data{ static_cast<T>(args)... } {}
-
-	T& operator[](size_t index) {
-		return data[index];
+	// Vector initializer lists
+	// Initializes a vector that owns all the data
+	VectorBase(std::initializer_list<T> values) requires (Owner) {
+		std::copy(values.begin(), values.end(), data.begin());
 	}
 
-	const T& operator[](size_t index) const {
-		return data[index];
+	// Initializes a vector that doesn't own the data
+	VectorBase(std::initializer_list<T*> values) requires (!Owner) {
+		std::copy(values.begin(), values.end(), data.begin());
 	}
+
+	// Variadic implementation for Owner == true
+	template <typename... Args>
+		requires (sizeof...(Args) == N && Owner)
+	VectorBase(Args&&... args) : data{ static_cast<T>(std::forward<Args>(args))... } {}
+
+	// Variadic implementation for Owner == false
+	template <typename... Args>
+		requires (sizeof...(Args) == N && !Owner)
+	VectorBase(Args&&... args) : data{ std::forward<Args>(args)... } {}
+
+	// FACTORIES
+
+	// Builds Vector across a strided block of memory (e.g. matrix row) using a start and stride length
+	static VectorBase<T, N, false> StridedAccess(T* start, size_t stride) {
+		VectorBase<T, N, false> vec;
+		for (size_t i = 0; i < N; i++) {
+			vec.setPointer(i, start + i * stride);
+		}
+		return vec;
+	}
+
+	void setPointer(size_t i, T* ptr) requires (!Owner) {
+		data[i] = ptr;
+	}
+
+	T& operator[](size_t i) {
+		if constexpr (Owner) {
+			return data[i];
+		}
+		else {
+			return *data[i];
+		}
+	}
+
+	const T& operator[](size_t i) const {
+		if constexpr (Owner) {
+			return data[i];
+		}
+		else {
+			return *data[i];
+		}
+	}
+
+	// Elementwise multiplication
+	template <Numeric Type, size_t Len, bool O>
+	VectorBase<T, N, Owner>& operator*=(VectorBase<Type, Len, O> vec) requires (Len == N) {
+		for (size_t i = 0; i < N; i++) {
+			operator[](i) *= vec[i];
+		}
+		return *this;
+	}
+
+	template <Numeric Type, size_t Len, bool O>
+	VectorBase<T, N, true> operator*(VectorBase<Type, Len, O> vec) requires (Len == N) {
+		VectorBase<T, N, true> ret;
+		for (size_t i = 0; i < N; i++) {
+			ret[i] = operator[](i) * vec[i];
+		}
+		return ret;
+	}
+
+	const T sum() const {
+		T sum{ 0 };
+		for (size_t i = 0; i < N; i++) {
+			sum += operator[](i);
+		}
+		return sum;
+	}
+
+
 
 	constexpr size_t size() const { return N; }
 private:
-	T data[N];
+	std::array<std::conditional_t<Owner, T, T*>, N> data;
 };
+
+template <Numeric T, size_t N>
+using Vector = VectorBase<T, N, true>;
+
+template <Numeric T, size_t N>
+using OwningVector = VectorBase<T, N, true>;
+
+template <Numeric T, size_t N>
+using PointerVector = VectorBase<T, N, false>;
+
 
 template <Numeric T>
 using Vector2D = Vector<T, 2>;
