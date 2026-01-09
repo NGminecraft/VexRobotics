b06e8a300edf2f92f2cfd2efb305bb0f6bbaf0a2
NGminecraft <minecrafterng399@gmail.com>
2026-01-09 09:07:11 -0700

    Stuff

--- src/Algorithms/InverseKinematics.cpp
+++ src/Algorithms/InverseKinematics.cpp
@@ -17,11 +17,11 @@ Vector<double, Joints> InverseKinematics(std::array<ArmSegment, Joints>& segment
 	std::array<Vector3D<double>*, Joints + 1> jointPositions;
 
 	rotationMatrices[0] = &orientation;
-	jointPositions[0] = new Vector3d<double>(0, 0, 0);
+	jointPositions[0] = new Vector3D<double>(0, 0, 0);
 	// Caching loop
 	for (size_t i = 1; i < Joints; i++) {
-		rotationMatrices[i] = new Matrix<double, 3, 3> rotationMatrices[i-1] * segments[i].getRotationAxis();
-		jointPositions[i] = new Vector3D<double> rotationMatrices[i] * segments[i].getRotationAxis().axis() * segments[i].getLength();
+		rotationMatrices[i] = rotationMatrices[i-1] * segments[i].getRotationAxis();
+		jointPositions[i] = rotationMatrices[i] * (segments[i].getRotationAxis().axis() * segments[i].getLength());
 	}
 
 	// MATH
