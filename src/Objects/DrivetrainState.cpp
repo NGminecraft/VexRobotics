0c1a395e815adf02caa7c16da2a7ac3ec7779ac5
NGminecrafter <grahanic001@alpinesd.org>
2026-01-27 21:28:54 -0700

    Added update method to DrivetrainState

--- src/Objects/DrivetrainState.cpp
+++ src/Objects/DrivetrainState.cpp
@@ -1,3 +1,7 @@
 #include "Objects/DrivetrainState.h"
 
 DrivetrainState::DrivetrainState(vex::drivetrain& dt) : StateObject<vex::drivetrain>(dt), velocity(dt) {}
+
+void DrivetrainState::update(unsigned long tick) {
+	velocity.update(tick);
+}
