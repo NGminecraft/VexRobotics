7f9141b6a7ad2a70f441340c6154226e81bfce5c
NGminecrafter <grahanic001@alpinesd.org>
2026-01-27 21:22:23 -0700

    Setting previous data point

--- src/Telemetry/TelemetryObjects/Drivetrain/DrivetrainVelocity.cpp
+++ src/Telemetry/TelemetryObjects/Drivetrain/DrivetrainVelocity.cpp
@@ -1,6 +1,12 @@
 #include "Telemetry/TelemetryObjects/Drivetrain/Dumb/DrivetrainVelocity.h"
 
 DrivetrainVelocity::DrivetrainVelocity(vex::drivetrain& drivetrain) : Telemetry<double>(), drivetrainRef(drivetrain) {
+	previousData = TelemetryDataPoint<double>::create(
+		0,
+		0.0,
+		vex::timer::system(),
+		newData()
+	);
 }
 
 double DrivetrainVelocity::newData() {
