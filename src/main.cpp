e6e3d9288a89b3f531e671f078d7139706af381b
NGminecraft <minecrafterng399@gmail.com>
2026-01-29 15:42:24 -0700

    Mo fixes

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@ vex::motor DriveRightMotor(vex::PORT2);
 vex::motor_group leftMotors(DriveLeftMotor);
 vex::motor_group rightMotors(DriveRightMotor);
 
-vex::drivetrain Drivetrain(leftMotors, rightMotors);
+vex::drivetrain Drivetrain(leftMotors, rightMotors, 319.19, 320, 40, vex::distanceUnits::mm, 1);
 DrivetrainState drivetrainState(Drivetrain);
 
 // define your global instances of motors and other devices here
