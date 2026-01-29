e96a4488a28ae33ba3d698b2b5568a53dd83080b
NGminecrafter <grahanic001@alpinesd.org>
2026-01-28 22:41:24 -0700

    Adding all the Telemetry parts

--- include/Objects/ControllerState.h
+++ include/Objects/ControllerState.h
@@ -5,7 +5,29 @@
 using controller = vex::controller;
 
 class ControllerState : public StateObject<controller> {
+public:
 	ControllerState(controller& c);
 
 	void update(const unsigned long tick) override;
+private:
+	// There got to have been a better way to do this...
+	Axis1Telemetry axis1;
+	Axis2Telemetry axis2;
+	Axis3Telemetry axis3;
+	Axis4Telemetry axis4;
+
+	ButtonATelemetry buttonA;
+	ButtonBTelemetry buttonB;
+	ButtonXTelemetry buttonX;
+	ButtonYTelemetry buttonY;
+
+	ButtonLeftTelemetry buttonLeft;
+	ButtonRightTelemetry buttonRight;
+	ButtonUpTelemetry buttonUp;
+	ButtonDownTelemetry buttonDown;
+
+	ButtonL1Telemetry buttonL1;
+	ButtonL2Telemetry buttonL2;
+	ButtonR1Telemetry buttonR1;
+	ButtonR2Telemetry buttonR2;
 };
--- src/Objects/ControllerState.cpp
+++ src/Objects/ControllerState.cpp
@@ -1,6 +1,25 @@
 #include "Objects/ControllerState.h"
 
 
-ControllerState::ControllerState(controller& c) : StateObject<controller>(c) {}
+ControllerState::ControllerState(controller& c) : StateObject<controller>(c),
+	axis1(c),
+	axis2(c),
+	axis3(c),
+	axis4(c),
+
+	buttonA(c),
+	buttonB(c),
+	buttonX(c),
+	buttonY(c),
+	buttonLeft(c),
+	buttonRight(c),
+	buttonUp(c),
+	buttonDown(c),
+
+	buttonL1(c),
+	buttonL2(c),
+	buttonR1(c),
+	buttonR2(c)
+{}
 
 void ControllerState::update(const unsigned long tick) {}
