7c5ba23d1247e049c04675fda24d33b3f7d0f7f1
NGminecraft <minecrafterng399@gmail.com>
2026-01-29 13:49:41 -0700

    Implementing driving

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,32 @@
 /*----------------------------------------------------------------------------*/
 #include "vex.h"
 #include "Cycle/MainLoop.h"
+#include "Cycle/Phases/UserPhase.h"
+#include "Objects/ControllerState.h"
+#include "Objects/DrivetrainState.h"
 using namespace vex;
 
 // A global instance of vex::brain used for printing to the V5 brain screen
 vex::brain       Brain;
 
+vex::controller Controller1;
+ControllerState controllerState(Controller1);
+
+vex::motor DriveLeftMotor(vex::PORT1);
+vex::motor DriveRightMotor(vex::PORT2);
+
+vex::drivetrain Drivetrain(DriveLeftMotor, DriveRightMotor);
+DrivetrainState drivetrainState(Drivetrain);
+
 // define your global instances of motors and other devices here
 
 int main() {
 	MainLoop mainLoop;
+
+	ArcadeMovement<ControllerAxis::Axis1, ControllerAxis::Axis2> movementLogic;
+	UserPhase<decltype(movementLogic)> userPhase(controllerState, drivetrainState);
+
+	mainLoop.registerPhase(userPhase);
 
 	// Build and register phases for the main loop here
 
