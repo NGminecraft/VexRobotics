57bc00311d74a0e3fe08f1fa2d732003fd03c848
NGminecraft <minecrafterng399@gmail.com>
2026-02-13 08:54:42 -0700

    Log level handling

--- include/Telemetry/Logging/LogMethods/Formatting/LogFormat.h
+++ include/Telemetry/Logging/LogMethods/Formatting/LogFormat.h
@@ -1,8 +1,15 @@
 #pragma once
+#include "Telemetry/Logging/Logger.h"
 #include <sstream>
 
 // Base class for log elements, things that add content to a log message
 class LogElement {
 public:
+	// Add content to the log message by writing to the stringstream
 	virtual void addElement(std::stringstream& ss) = 0;
+
+	// By default ignore log level, but some elements may want to change that
+	virtual void addElement(std::stringstream& ss, Logger::LogLevel level) {
+		addElement(ss);
+	}
 };
--- src/Telemetry/Logging/Logger.cpp
+++ src/Telemetry/Logging/Logger.cpp
@@ -27,7 +27,34 @@ Logger& Logger::getInstance() {
 	return *instance;
 }
 
-void Logger::log(const char* msg) {
-	if (msg != nullptr) {
+void Logger::log(const char* msg, Logger::LogLevel level) {
+	// Call all the simple handles first
+	for (auto handle : simpleHandles) {
+		handle->log(msg);
 	}
+
+	// Stringstream to build the log message
+	std::stringstream ss;
+	
+	// Prepend elements
+	for (auto element : prependedElements) {
+		element->addElement(ss, level);
+	}
+
+	// Append message
+	ss << msg;
+
+	// Append elements
+	for (auto element : appendedElements) {
+		element->addElement(ss, level);
+	}
+
+	// Call handles
+	for (auto handle : handles) {
+		handle->log(ss.str().c_str());
+	}
+}
+
+void Logger::log(const char* msg) {
+	log(msg, defaultLevel);
 }
