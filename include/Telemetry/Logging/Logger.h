08f02a68077825b5f73fdb7a3873097ff0b513fd
NGminecraft <minecrafterng399@gmail.com>
2026-02-13 08:44:55 -0700

    Comments!

--- include/Telemetry/Logging/Logger.h
+++ include/Telemetry/Logging/Logger.h
@@ -14,16 +14,25 @@ public:
 		CRITICAL
 	};
 
+	// Get (or create) the singleton instance of the logger
 	static Logger& getInstance(const char* Name);
 	static Logger& getInstance(const char* Name, LogLevel defaultLevel);
 	static Logger& getInstance();
 
+	// Log a message with the default log level
 	void log(const char* message);
+
+	// Log a message with a specific log level
 	void log(const char* message, LogLevel level);
 
+
+	// Add an element to the beginning of a log message
 	void prependElement(LogElement* element);
+
+	// Add an element to the end of a log message
 	void appendElement(LogElement* element);
 	
+
 	// Registering a simple handle, or a handle that doesn't contain any formatting elements
 	void addSimpleHandle(LogHandle* callback);
 
@@ -37,6 +46,9 @@ private:
 	Logger(const char* Name, LogLevel defaultLevel);
 
 	LogLevel defaultLevel;
+
+	std::vector<LogHandle*> simpleHandles;
+	std::vector<LogHandle*> handles;
 
 	std::vector<LogElement*> prependedElements;
 	std::vector<LogElement*> appendedElements;
