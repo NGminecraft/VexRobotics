065f839b7c789d5a7f0bbf77a58863bc772a5498
NGminecraft <minecrafterng399@gmail.com>
2026-02-13 08:28:50 -0700

    Cleaning up .cpp file

--- src/Telemetry/Logging/Logger.cpp
+++ src/Telemetry/Logging/Logger.cpp
@@ -3,40 +3,31 @@
 
 Logger* Logger::instance = nullptr;
 
-Logger::Logger(vex::brain& brain, const char* Name) : brainRef(brain), name(Name) {
+Logger::Logger(const char* Name) : name(Name), defaultLevel(Logger::LogLevel::DEBUG) {}
 
-}
+Logger::Logger(const char* Name, LogLevel defaultLevel) : name(Name), defaultLevel(defaultLevel) {}
 
-Logger& Logger::getInstance(vex::brain& brain, const char* Name) {
+Logger& Logger::getInstance(const char* Name) {
 	// FIX 3: Use static instance instead of local variable
 	if (instance == nullptr) {
-		static Logger logInstance(brain, Name);
+		static Logger logInstance(Name);
 		instance = &logInstance;
 	}
 	return *instance;
 }
 
 Logger& Logger::getInstance() {
-	// FIX 4: Add null check to prevent dereferencing null pointer
 	if (instance == nullptr) {
 		// This is a critical error - getInstance(brain, name) must be called first
 		// For embedded systems, we can't throw exceptions, so we need to handle this gracefully
 		// You should call the two-parameter version first during initialization
-		vex::brain dummyBrain;
-		static Logger defaultInstance(dummyBrain, "DefaultLogger");
+		static Logger defaultInstance("DefaultLogger");
 		instance = &defaultInstance;
 	}
 	return *instance;
 }
 
 void Logger::log(const char* msg) {
-	// FIX 5: Add null check before logging
 	if (msg != nullptr) {
-		brainRef.Screen.print(msg);
-		brainRef.Screen.newLine();
 	}
-}
-
-void Logger::clear() {
-	brainRef.Screen.clearScreen();
 }
