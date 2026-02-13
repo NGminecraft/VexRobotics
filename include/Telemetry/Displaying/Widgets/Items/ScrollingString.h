3b70b518b232557e510992d0cd62a54c47a2e8b2
NGminecraft <minecrafterng399@gmail.com>
2026-02-13 08:11:20 -0700

    ScrollingString class

---
+++ include/Telemetry/Displaying/Widgets/Items/ScrollingString.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Telemetry/Displaying/ScreenItem.h"
+
+template <size_t N>
+class ScrollingString : public ScreenItem {
+public:
+	ScrollingString(int x, int y) : offsetX(x), offsetY(y) {};
+
+	void addString(const char* str) {
+		buffer[currentIndex] = str;
+		currentIndex = (currentIndex + 1) % N;
+	}
+
+	void print(vex::brain& brain, int x, int y) override {
+		for (size_t i = 0; i < N; i++) {
+			size_t idx = (currentIndex + i) % N;
+			if (buffer[idx]) {
+				brain.Screen.printAt(x + offSetX, y + i + offsetY, buffer[idx]);
+			}
+		}
+	}
+
+protected:
+	const char* buffer[N];
+	size_t currentIndex = 0;
+
+	int offsetX;
+	int offsetY;
+};
